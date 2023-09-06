5f54e501c13d6c4e500eaf0d7cfe7ad89ed9e595
Alejandro Tito <aristides600@gmail.com>
2023-09-06 20:25:58 -0300

    fecha

---
+++ fecha.php
@@ -0,0 +1,14 @@
+<?php
+$prueba = date("Y/m/d");
+echo "Hoy es: ".date("d/m/Y");
+echo "<br/>";
+
+if ($prueba < "2020/12/01")
+{
+echo "Sitio web en mantenimiento hasta el 01/12/2020";
+}
+else
+{
+echo "Bienvenido a nuestro sitio!";
+}
+?>
